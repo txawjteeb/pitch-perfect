97cf67d2eb5313cb6b0669b69b208908953c8c67
David Lee <itxawjteeb@gmail.com>
2015-06-02 20:02:30 -0700

    First iOS App Voice recorder

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,16 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by David Lee on 5/23/15.
+//  Copyright (c) 2015 David Lee. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    
+    var filePathUrl: NSURL!
+    var title: String!
+    
+}
